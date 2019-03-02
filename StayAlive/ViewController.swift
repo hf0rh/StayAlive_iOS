ea091178eb783981630e661087c8365f17f40a05
Kyle Fenole <kyle@kfenole.com>
2019-03-02 11:13:55 -0800

    Initial Commit

---
+++ StayAlive/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StayAlive
+//
+//  Created by Kyle Fenole on 3/2/19.
+//  Copyright © 2019 Kyle Fenole. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
